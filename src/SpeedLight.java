08920939a89e8d493fa5f2c0f1c7b09f43ea7245
Olufunmilayo Ekpereh <oekpereh@ggc.edu>
2023-01-26 21:19:24 -0500

    added comment line 14

---
+++ src/SpeedLight.java
@@ -0,0 +1,16 @@
+/**
+ * @Class: SpeedLight
+ * @author: Olufunmilayo Ekpereh
+ * version 1.0
+ * Course: ITEC 2140 Spring 2023
+ * Written: February 26, 2023
+ *
+ * This class is calculating distance to a lightning strike
+ */
+
+
+public class SpeedLight {
+    public static void main(String [] args) {
+//class completed
+    }
+}
