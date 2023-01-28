6ce53beb4b89bbd98af89a7fd1fc6747eba0d0a8
Olufunmilayo Ekpereh <oekpereh@ggc.edu>
2023-01-27 23:50:37 -0500

    Stamps.java created

---
+++ src/Stamps.java
@@ -0,0 +1,18 @@
+/**
+ * @Class: Stamps
+ * @author: Olufunmilayo Ekpereh
+ * version 1.0
+ * Course: ITEC 2140 Spring 2023
+ * Written: February 27, 2023
+ *
+ * This class is computing the difference between collections of stamps
+ */
+
+public class Stamps {
+    public static void main(String[] args) {
+        int Susan = 8;
+        int Jean = 40;
+        int numb_stamps = Jean - Susan;
+        System.out.println("Jean has "+numb_stamps + " more stamps than Susan.");
+    }
+}
