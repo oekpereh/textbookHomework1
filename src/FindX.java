4b7b00fa7431ba81dc36d3def68a5f000327c519
Olufunmilayo Ekpereh <oekpereh@ggc.edu>
2023-02-02 23:27:08 -0500

    comments added in program

---
+++ src/FindX.java
@@ -0,0 +1,20 @@
+/**
+ * @Class: FindX
+ * @author: Olufunmilayo Ekpereh
+ * version 1.0
+ * Course: ITEC 2140 Spring 2023
+ * Written: February 2, 2023
+ *
+ * This program will compute the number X based on formula.
+ * In this program, I will declare variables: X, c, y to find X.
+ */
+
+public class FindX {
+    public static void main(String[] args) {
+        int X;
+        int y = 5 + 19 + 47;
+        int c = 194; //total number
+        X = c-y; //this value will find X
+        System.out.println("The result of X is " + X);
+    }
+}
