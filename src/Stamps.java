5299180da76330e47813c76b899516575d6fc1a3
Olufunmilayo Ekpereh <oekpereh@ggc.edu>
2023-02-02 22:35:05 -0500

    class comment added

--- src/Stamps.java
+++ src/Stamps.java
@@ -3,16 +3,18 @@
  * @author: Olufunmilayo Ekpereh
  * version 1.0
  * Course: ITEC 2140 Spring 2023
- * Written: February 27, 2023
+ * Written: February 2, 2023
  *
- * This class is computing the difference between collections of stamps
+ * This program will compute the difference between Jean's and Susan's
+ * collections of stamps.
+ * In this program, I will declare variables: Susan, Jean, numb of stamps.
  */
 
 public class Stamps {
     public static void main(String[] args) {
         int Susan = 8;
         int Jean = 40;
-        int numb_stamps = Jean - Susan;
-        System.out.println("Jean has "+numb_stamps + " more stamps than Susan.");
+        int numb_of_stamps = Jean - Susan;
+        System.out.println("Jean has "+numb_of_stamps + " more stamps than Susan.");
     }
 }
