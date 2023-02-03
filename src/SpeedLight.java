a54954ec8d3688cc7bdd7fe95c2f5854b6300e6f
Olufunmilayo Ekpereh <oekpereh@ggc.edu>
2023-02-02 22:13:16 -0500

    class comment added

--- src/SpeedLight.java
+++ src/SpeedLight.java
@@ -3,18 +3,22 @@
  * @author: Olufunmilayo Ekpereh
  * version 1.0
  * Course: ITEC 2140 Spring 2023
- * Written: February 26, 2023
+ * Written: February 2, 2023
  *
- * This class is calculating distance to a lightning strike
+ * This program will calculate the distance to a lightning strike based on the time elapsed
+ * between the flash and the sound of thunder
+ * In this program, I will declare variables: speedOfSound, timeElapsed, distance
  */
 
 
 public class SpeedLight {
     public static void main(String [] args) {
 //class completed
-        float speed = 340;
-        float seconds = 5;
-        float distance = speed * seconds;
-        System.out.println("The distance: " + distance + " meters");
+        int SPEED_OF_SOUND = 340; //this value will never change
+        int timeElapsed = 5;
+        int distance = SPEED_OF_SOUND * timeElapsed;
+        System.out.println("Speed of sound is: " + SPEED_OF_SOUND);
+        System.out.println("Time Elapsed is: " + timeElapsed);
+        System.out.println("The distance to a lightning strike is: " + distance);
     }
 }
